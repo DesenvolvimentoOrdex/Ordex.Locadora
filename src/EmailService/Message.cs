017ea3927c33c0ed3922b0a74dfc7e101848d7f5
Yam <Yam@DESKTOP-T7TRNGC>
2024-04-21 09:50:20 -0300

    Feat#3 Email service

---
+++ src/EmailService/Message.cs
@@ -0,0 +1,27 @@
+﻿using EmailService;
+using Microsoft.AspNetCore.Http;
+using MimeKit;
+
+
+
+namespace Ordex.Locadora.Service.EmailService
+{
+    public class Message
+    {
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public IFormFileCollection Attachments { get; set; }
+
+        public Message(IEnumerable<MailboxAddress> to, string subject, string content, IFormFileCollection attachments)
+        {
+            To = new List<MailboxAddress>();
+
+            To.AddRange(to.Select(x => new MailboxAddress(x.Name, x.Address)));
+            Subject = subject;
+            Content = content;
+            Attachments = attachments;
+        }
+    }
+}
